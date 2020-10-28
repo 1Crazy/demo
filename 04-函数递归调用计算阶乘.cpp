dd7b51d2ac8a30eb84e6a1b2ae7b5fb99ee3219a
kyle <wangyuhong7777@163.com>
2020-10-28 13:22:57 +0800

    递归调用计算阶乘

---
+++ 04-函数递归调用计算阶乘.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>;
+using namespace std;
+
+unsigned fac(unsigned n) {
+	if (n == 0) {
+		return 1;
+	}
+	else {
+		return fac(n - 1) * n;
+	}
+}
+
+int main() {
+	unsigned n;
+
+	cout << "请输入一个正整数：";
+	cin >> n;
+
+	unsigned y = fac(n);
+
+	cout << n << "!为" << y << endl;
+	return n;
+}
