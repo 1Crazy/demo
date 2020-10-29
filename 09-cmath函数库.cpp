1b7d70cc3d5ee385fd74ff78e927811f60548f02
kyle <wangyuhong7777@163.com>
2020-10-29 16:30:31 +0800

    cmath函数库的使用

---
+++ 09-cmath函数库.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>;
+#include <cmath>;
+using namespace std;
+
+int main() {
+	int x = 3;
+	int y = 2;
+
+	int z = pow(x, y);
+	
+	cout << "x为3，y为2，则x得y次幂为" << z << "\n";
+	
+	return 0;
+}
